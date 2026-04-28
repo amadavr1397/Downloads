024ec61ac81a19d7c8b2c370d7eacf3b32c543d8
amadavr1397 <38191672+amadavr1397@users.noreply.github.com>
2026-04-28 11:31:42 +0330

    Update YDbot.py

--- YDbot.py
+++ YDbot.py
@@ -646,9 +646,9 @@ async def handle_callback(callback_query):
                         
             # [a, b] = users_query[users_query['user_id'] == str(user_id)]['band'].to_list()[0]
             # [a, b] = users_band[users_band['user_id'] == str(user_id)]['band'].to_list()[0]
-            # [a,b] = users_band[users_band['user_id'] == str(user_id)]['band'].to_list()[0]
+            [a,b] = users_band[users_band['user_id'] == str(user_id)]
             
-            print(users_band)
+            print(users_band,a,b)
             
             # await yt_search(user_id, message_id, '', 50, a, b)
 

2f89918bcb3436d62fea70a6a748900aa6f38a60
amadavr1397 <38191672+amadavr1397@users.noreply.github.com>
2026-04-29 03:55:31 +0330

    Update YDbot.py

--- YDbot.py
+++ YDbot.py
@@ -71,7 +71,7 @@ async def search_query(queue, user_id, search, number=5, a=0, b=5):
             'quiet': True,
             'skip_download': True,
             'extract_flat': False,   # fast, no full extraction
-            'js_runtime': 'node',
+            'js_runtime': '/root/node-v22.14.0-linux-x64/bin/node',
             'cookiefile': 'YTDLnis_Cookies.txt',
         }
         

cb6b7f3241b7b87e781a5d1a14521db9d976d1e3
amadavr1397 <38191672+amadavr1397@users.noreply.github.com>
2026-04-30 11:36:01 +0330

    Update YDbot2.py

--- YDbot2.py
+++ YDbot2.py
@@ -322,8 +322,7 @@ async def yt_search(user_id, query_title, number=5, a=0, b=5):
 async def make_progress_spinner(message, stop_event=False):
     
     msg = await Client.send_message(message.chat.id,
-                              message_id=message.id,
-                              text='⠋')
+                                    text='⠋')
     
     for char in itertools.cycle('⠋⠙⠹⠼⠴⠦⠧⠏'):
     
